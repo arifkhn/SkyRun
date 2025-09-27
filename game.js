fb38a0e1fd24eda23b2d9730cde26df5278f0ef4
Arif khan <127328010+arifkhn@users.noreply.github.com>
2025-09-27 15:44:43 +0530

    Update game.js

--- game.js
+++ game.js
@@ -145,6 +145,20 @@ document.addEventListener('keydown', e => {
   }
 });
 
+document.addEventListener("touchstart", e => {
+  touchStartX = e.changedTouches[0].screenX;
+  touchStartY = e.changedTouches[0].screenY;
+});
+
+document.addEventListener("touchend", e => {
+  touchEndX = e.changedTouches[0].screenX;
+  touchEndY = e.changedTouches[0].screenY;
+  handleSwipe();
+});
+
+
+
+
 function checkCollision() {
   for (let p of platforms) {
     if (
